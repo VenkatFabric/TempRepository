65f99c3f81ea5b2b06a451158fb0a691fb14b8a9
VenkatFabric <GFarbicDemo@vgad222.onmicrosoft.com>
2026-01-03 07:50:35 +0530

    New Notebook added

---
+++ DWSalesDevelopment/SalesNotebook.Notebook/notebook-content.py
@@ -0,0 +1,24 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+EmpId = 101
+EmpName = "Venkat"
+print(EmpId)
+print(EmpName)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
